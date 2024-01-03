52ec9d92aa9da9f7a0de643dcab1382d7389c9c7
0Huns <dudgns3864@gmail.com>
2024-01-03 21:27:28 +0900

    downtab_scroll

--- javascript.js
+++ javascript.js
@@ -2,3 +2,15 @@
 window.onload = setTimeout(function(){
     document.querySelector('.section_1_detail').style.opacity = "1";
 }, 1000);
+
+//다운 탭 클릭 시 화면 이동
+let down = document.querySelector('.section_1_downTab');
+let section2 = document.querySelector('.section_base_even');
+
+function downtab(event){
+    event.preventDefault();
+
+    window.scrollBy( 0, section2.getBoundingClientRect().top - 60 );
+}
+
+down.addEventListener('click', downtab);
